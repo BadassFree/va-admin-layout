8584a6c69ab157be0936acb8ec6b8b8e2cc7ba8c
villain <1423782121@qq.com>
2023-01-31 15:32:04 +0800

    第一版

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,78 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <admin-layout
+    :mode="mode"
+    :is-mobile="isMobile"
+    :fixed-header-and-tab="fixedHeaderAndTab"
+    :fixed-footer="fixedFooter"
+    :sider-collapse="siderCollapse"
+    @update:sider-collapse="setSiderCollapse"
+  >
+    <template #header>
+      <div class="flex-center h-full bg-#e6e6e6">Header</div>
+    </template>
+    <template #tab>
+      <div class="flex-center h-full bg-#cccccc">Tab</div>
+    </template>
+    <template #sider>
+      <div class="h-full bg-#d9d9d9">
+        <div class="flex-center h-56px">Sider</div>
+      </div>
+    </template>
+    <template #footer>
+      <div class="flex-center h-full bg-#e6e6e6">Footer</div>
+    </template>
+    <div class="fixed right-0 top-120px px-12px whitespace-nowrap">
+      <div>
+        <h4>layout mode:</h4>
+        <div v-for="item in modeList" :key="item">
+          <span class="pr-8px">{{ item }}</span>
+          <input
+            type="radio"
+            name="mode"
+            :value="item"
+            :checked="item === mode"
+            class="cursor-pointer"
+            @change="setMode(item)"
+          />
+        </div>
+      </div>
+      <div class="pt-24px">
+        <span class="pr-8px">isMobile</span>
+        <input type="checkbox" :checked="isMobile" @change="toggleIsMobile" />
+      </div>
+      <div class="pt-24px">
+        <span class="pr-8px">fixedHeaderAndTab</span>
+        <input type="checkbox" :checked="fixedHeaderAndTab" @change="toggleFixedHeaderAndTab" />
+      </div>
+      <div class="pt-24px">
+        <span class="pr-8px">fixedFooter</span>
+        <input type="checkbox" :checked="fixedFooter" @change="toggleFixedFooter" />
+      </div>
+      <div class="pt-24px">
+        <span class="pr-8px">siderCollapse</span>
+        <input type="checkbox" :checked="siderCollapse" @change="toggleSiderCollapse" />
+      </div>
+    </div>
+    <div v-for="i in 50" :key="i" class="text-center">{{ i }}</div>
+  </admin-layout>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useBoolean } from '@/hooks';
+import AdminLayout from './index.vue';
+
+type Mode = 'vertical' | 'horizontal';
+const mode = ref<Mode>('vertical');
+const modeList: Mode[] = ['vertical', 'horizontal'];
+function setMode(value: Mode) {
+  mode.value = value;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+const { bool: isMobile, toggle: toggleIsMobile } = useBoolean();
+const { bool: fixedHeaderAndTab, toggle: toggleFixedHeaderAndTab } = useBoolean(true);
+const { bool: fixedFooter, toggle: toggleFixedFooter } = useBoolean();
+const { bool: siderCollapse, setBool: setSiderCollapse, toggle: toggleSiderCollapse } = useBoolean();
+</script>
+
+<style></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,66 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Author: 蚊子
+ * @Date: 2023-01-31 11:32:59
+ * @LastEditTime: 2023-01-31 14:59:42
+ * @LastEditors: 蚊子
+ * @Description: 
+ * @FilePath: /va-admin-layout/vite.config.ts
+ * 坚持就是........
+ */
+import { fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+import DefineOptions from 'unplugin-vue-define-options/dist/vite';
+import unocss from 'unocss/vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+
+export default defineConfig(configEnv => {
+  const viteEnv = loadEnv(configEnv.mode, `.env.${configEnv.mode}`);
+
+  const isVercel = viteEnv.VITE_IS_VERCEL === '1';
+
+  return {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    plugins: [
+      vue(),
+      DefineOptions(),
+      unocss({ include: ['src/App.vue'] }),
+      dts({
+        include: ['./src/index.ts', './src/index.vue'],
+        beforeWriteFile(filePath, content) {
+          return {
+            filePath: filePath.replace('/dist/src/', '/dist/'),
+            content
+          };
+        }
+      })
+    ],
+    optimizeDeps: {
+      exclude: ['vue-demi']
+    },
+    build: isVercel
+      ? {
+          brotliSize: false
+        }
+      : {
+          lib: {
+            entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+            name: 'BadassAdminLayout',
+            fileName: 'index'
+          },
+          rollupOptions: {
+            external: ['vue'],
+            output: {
+              globals: {
+                vue: 'Vue'
+              }
+            }
+          }
+        }
+  };
+});
